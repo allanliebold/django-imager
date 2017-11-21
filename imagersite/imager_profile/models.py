b531f9e1957205fcc7f9205cd705f038ae1f2cec
Darren-Haynes <darrenhaynes@zoho.com>
2017-11-20 17:05:42 -0800

    added more fields to ImagerProfile model

--- imagersite/imager_profile/models.py
+++ imagersite/imager_profile/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-
 
 class ImageActiveProfile(models.Manager):
     """."""
@@ -13,8 +11,48 @@ class ImageActiveProfile(models.Manager):
 
 
 class ImagerProfile(models.Model):
+    """Imager Profile Model."""
+
+    CAMERAS = (
+        ('NK', 'Nikon'),
+        ('OL', 'Olympus'),
+        ('GP', 'Go-Pro'),
+    )
+
+    SERVICES = (
+        ('WD', 'Wedding'),
+        ('GD', 'Graduation'),
+        ('SP', 'Active/Sports'),
+    )
+
+    PHOTOGRAPHY = (
+        ('BW', 'Black and White'),
+        ('CL', 'Color')
+    )
+
+    website = models.URLField(default="example.com")
     location = models.CharField(max_length=200, blank=True, null=True)
     user = models.OneToOneField(User, related_name='profile')
+    fee = models.FloatField(default=0.0)
+    phone = models.CharField(default="555-555-5555", max_length=100)
+
+    camera = models.CharField(
+        max_length=2,
+        choices=CAMERAS,
+        default='NIKON'
+    )
+
+    services = models.CharField(
+        max_length=2,
+        choices=SERVICES,
+        default='WEDDING'
+    )
+
+    photo_styles = models.CharField(
+        max_length=2,
+        choices=PHOTOGRAPHY,
+        default='CL'
+    )
 
     active = ImageActiveProfile()
 
