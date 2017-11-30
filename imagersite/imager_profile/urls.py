34c1b27b0cd06bb91b561adc5d11efab6ed645bd
Darren-Haynes <darrenhaynes@zoho.com>
2017-11-30 15:53:39 -0800

    added urls for user profile page.

--- imagersite/imager_profile/urls.py
+++ imagersite/imager_profile/urls.py
@@ -1,8 +1,11 @@
 """imagersite URL Configuration
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
+from . import views
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.profile_view, name="profile")
 ]
--- imagersite/imagersite/urls.py
+++ imagersite/imagersite/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     url(r'^login/$', auth_views.login, name='login'),
     url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
     url(r'^accounts/', include('registration.backends.hmac.urls')),
-    # url(r'^accounts/login$', 'django.contrib.auth.views.login', name='accounts-login'),
+    url(r'^profile/', include('imager_profile.urls')),
 ]
 
 if settings.DEBUG:
