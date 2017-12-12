0129290c731e4bfbdb2030c5690f34ae0c272cf9
Darren-Haynes <darrenhaynes@zoho.com>
2017-12-12 11:08:12 -0800

    added receiver function

--- imagersite/imager_profile/models.py
+++ imagersite/imager_profile/models.py
@@ -1,5 +1,8 @@
-from django.db import models
+"""Profile Models."""
 from django.contrib.auth.models import User
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class ImageActiveProfile(models.Manager):
@@ -61,3 +64,11 @@ class ImagerProfile(models.Model):
     def is_active(self):
         """."""
         return self.user.is_active
+
+
+@receiver(post_save, sender=User)
+def create_profile(sender, **kwargs):
+    """."""
+    if kwargs['created']:
+        profile = ImagerProfile(user=kwargs['instance'])
+        profile.save()
